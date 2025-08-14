5a404ac681d95b6a7fd86be74eb7a8aae16cb115
Meekaaeel Booley <blymee001@myuct.ac.za>
2025-08-14 20:01:39 +0200

    First commit. Setup React+Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import intropic from './assets/nuts.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={intropic} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Deez Nuts</h1>
+    </>
+  )
+}
+
+export default App
